6805067c30b58415c3a6f5c0574207721e857be0
Gijs Wassenaar <mail@gijswassenaar.nl>
2014-10-07 09:50:07 +0200

    Added roll-a-ball project Gijs

---
+++ Roll-a-ball-Gijs/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private bool isGrounded = true;
+	public float speed;
+	public float JumpSpeed = 100.0f;
+	
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		//float moveUp = Input.GetAxis ("Jump");
+		//float moveUp = Input.GetKey (KeyCode.Space);
+		
+		//Vector3 jumper = new Vector3 (1, 1, 1);
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigidbody.AddForce(movement * speed * Time.deltaTime);
+		
+		if(Input.GetKeyUp (KeyCode.Space) && isGrounded) {
+			rigidbody.AddForce(Vector3.up * JumpSpeed);
+			isGrounded = false;
+		}
+	}
+
+	void OnCollisionEnter (Collision hit)
+	{
+		if(hit.gameObject.tag == "Ground")
+		{
+			isGrounded = true;
+		}
+	}
+}
---
+++ Roll-a-ball-Gijs/Assets/Scripts/RandomSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomSpawner : MonoBehaviour
+{
+	public GameObject[] spawnObject;    //somehow change this to incorporate multiple gameobject prefabs, will an array support that?
+	
+	//Would I create public variables for each prefab I want to be randomly chosen from, or would those be contained in the array above?
+	
+	public float xRange = 1.0f;
+	public float yRange = 1.0f;
+	public float minSpawnTime = 1.0f;
+	public float maxSpawnTime = 10.0f;
+	
+	void Start()
+	{
+		InvokeRepeating("SpawnObject", Random.Range(minSpawnTime,maxSpawnTime), 0.3f);
+	}
+	
+	void SpawnObject()
+	{
+		float xOffset = Random.Range(-xRange, xRange);
+		float yOffset = Random.Range(-yRange, yRange);
+		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube.AddComponent<Rigidbody>();
+		Rigidbody rigidcube = cube.rigidbody;
+		Vector3 pos = new Vector3 (0, 1, 0);
+		Rigidbody newObstacle = GameObject.Instantiate(rigidcube,pos, Quaternion.identity) as Rigidbody;
+	}
+}
