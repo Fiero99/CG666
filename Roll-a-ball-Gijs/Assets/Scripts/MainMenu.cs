64894ef40e70fa243b8feb849eb3734bc20d9e28
amarag <username@users.noreply.github.com>
2014-10-07 22:55:12 +0200

    Start on main menu
    Started making a main menu.

---
+++ Roll-a-ball-Gijs/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	public bool isQuit = false;
+
+	void OnMouseEnter (){
+		renderer.material.color = Color.red;
+	}
+
+	void OnMouseExit(){
+		renderer.material.color = Color.green;
+	}
+
+	void OnMouseDown(){
+		if (isQuit) {
+			Application.Quit ();
+		} else {
+			Application.LoadLevel(0);
+		}
+	}
+}
