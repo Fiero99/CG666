299b27fb856db3e877fb16f217460b5017230096
Gijs Wassenaar <mail@gijswassenaar.nl>
2014-10-08 09:10:59 +0200

    small code cleanup

--- Roll-a-ball-Gijs/Assets/Scripts/PlayerController.cs
+++ Roll-a-ball-Gijs/Assets/Scripts/PlayerController.cs
@@ -16,14 +16,10 @@ public class PlayerController : MonoBehaviour {
 	void FixedUpdate () {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
-		//float moveUp = Input.GetAxis ("Jump");
-		//float moveUp = Input.GetKey (KeyCode.Space);
-		
-		//Vector3 jumper = new Vector3 (1, 1, 1);
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		rigidbody.AddForce(movement * speed * Time.deltaTime);
 		
-		if(Input.GetKeyUp (KeyCode.Space) && isGrounded) {
+		if(Input.GetKeyDown (KeyCode.Space) && isGrounded) {
 			rigidbody.AddForce(Vector3.up * JumpSpeed);
 			isGrounded = false;
 		}
--- Roll-a-ball-Gijs/Assets/Scripts/RandomSpawner.cs
+++ Roll-a-ball-Gijs/Assets/Scripts/RandomSpawner.cs
@@ -3,12 +3,6 @@ using System.Collections;
 
 public class RandomSpawner : MonoBehaviour
 {
-	public GameObject[] spawnObject;    //somehow change this to incorporate multiple gameobject prefabs, will an array support that?
-	
-	//Would I create public variables for each prefab I want to be randomly chosen from, or would those be contained in the array above?
-	
-	public float xRange = 1.0f;
-	public float yRange = 1.0f;
 	public float minSpawnTime = 1.0f;
 	public float maxSpawnTime = 2.0f;
 	public float spawnSpeed = 4.0f;
@@ -20,14 +14,11 @@ public class RandomSpawner : MonoBehaviour
 	
 	void SpawnObject()
 	{
-		float xOffset = Random.Range(-xRange, xRange);
-		float yOffset = Random.Range(-yRange, yRange);
 		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
 		cube.AddComponent<Rigidbody>();
 		Rigidbody rigidcube = cube.rigidbody;
 		Vector3 pos = new Vector3 (0, 1, 0);
 		cube.transform.position = pos;
-		//Rigidbody newObstacle = GameObject.Instantiate(rigidcube,pos, Quaternion.identity) as Rigidbody;
 		rigidcube.renderer.material.color = Color.red;
 		rigidcube.velocity = new Vector3(30, 0, 0);
 	}
