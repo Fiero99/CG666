a58632aac05a514ac59d6a4c982934b251215ebc
TechnoJunk12 <username@users.noreply.github.com>
2014-10-14 22:27:32 +0200

    verbeterde spawning

--- Roll-a-ball-Gijs/Assets/Scripts/RandomSpawner.cs
+++ Roll-a-ball-Gijs/Assets/Scripts/RandomSpawner.cs
@@ -9,19 +9,20 @@ public class RandomSpawner : MonoBehaviour
 	public float spawnSpeed = 1.0f;
 	public GameObject[] spawns;
 	public Vector3 startloc; // the starting location of the block
-	public float direction; // pos = going  right, neg = going left
+	public Vector3 endloc;
+	public int speed;
 
-
+	
 	void Start()
 	{
 		print ("RandomSpawn1");
-		InvokeRepeating("randomizer", 0, 1.3f);
+		InvokeRepeating("randomizer", 0, 2f);
 	}
 
 	void randomizer()
 	{
 		//float delay = Random.Range (0, 0.01f);
-		Invoke ("SpawnObject", 6f);
+		Invoke ("SpawnObject", 0);
 
 	}
 
@@ -32,21 +33,24 @@ public class RandomSpawner : MonoBehaviour
 		GameObject cube = GameObject.Instantiate(car) as GameObject;
 		Rigidbody rigidcube = cube.rigidbody;
 		rigidcube.tag = ("Vehicle");
-		//rigidcube.isKinematic = true;
+		rigidcube.isKinematic = true;
 		cube.transform.position = startloc;
 	}
 
 
 	// find the vehicle objects and move them one step to the direction
 	void Update () {
-		spawns = GameObject.FindGameObjectsWithTag("Vehicle");
+		        Vector3 diff = (endloc - startloc)/100;
+				spawns = GameObject.FindGameObjectsWithTag ("Vehicle");
 
-		for(int i = 0; i < spawns.Length; i++)
-		{
-			spawns[i].transform.eulerAngles = new Vector3(0, 0, 0);
-			spawns[i].transform.Translate(new Vector3 (0.19f, 0, 0)*direction);
+				for (int i = 0; i < spawns.Length; i++) {
+						spawns [i].transform.eulerAngles = new Vector3 (0, 0, 0);
+						spawns [i].transform.Translate (diff* speed);
+						if (spawns [i].transform.position.x > 12) {
+								Destroy (spawns [i]);
+						}
+
+				}
 		}
-
-	}
 
 }
