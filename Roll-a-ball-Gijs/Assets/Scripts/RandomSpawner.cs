b7fab09be6cd4b7b2c9104dfc4d1e51187c611b3
TechnoJunk12 <username@users.noreply.github.com>
2014-10-19 20:07:25 +0200

    Missing update?

--- Roll-a-ball-Gijs/Assets/Scripts/RandomSpawner.cs
+++ Roll-a-ball-Gijs/Assets/Scripts/RandomSpawner.cs
@@ -6,8 +6,6 @@ public class RandomSpawner : MonoBehaviour
 {
 	public GameObject[] spawnObject;
 	public GameObject car;
-	public float minSpawnTime = 0.1f;
-	public float spawnSpeed = 1.0f;
 	public List<GameObject> spawns;
 	public Vector3 startloc; // the starting location of the block
 	public Vector3 endloc;
@@ -18,13 +16,13 @@ public class RandomSpawner : MonoBehaviour
 	
 	void Start()
 	{
-		float delay = Random.Range (5* speed, 30*speed);
+		float delay = Random.Range (2* speed, 25*speed);
 		Invoke("randomizer", delay);
 	}
 	
 	void randomizer()
 	{
-		float delay = Random.Range ( 0.5f, 1f + 0.2f/speed);
+		float delay = Random.Range ( 0.1f/speed, 1f + 0.3f/speed);
 		print (delay);
 		Invoke ("SpawnObject", delay);		
 	}
@@ -37,7 +35,7 @@ public class RandomSpawner : MonoBehaviour
 		rigidcube.isKinematic = true;
 		cube.transform.position = startloc;
 		spawns.Add (cube);
-		float delay = Random.Range ( 0.5f, 1f + 0.2f/speed);
+		float delay = Random.Range ( 0.1f/speed, 1f + 0.2f/speed);
 		Invoke ("randomizer", delay);	
 	}
 
