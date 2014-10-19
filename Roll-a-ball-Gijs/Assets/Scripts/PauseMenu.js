507e2455f1545a1d38dd24577195228618d643e9
amarag <username@users.noreply.github.com>
2014-10-19 21:11:15 +0200

    added pauseMenu

---
+++ Roll-a-ball-Gijs/Assets/Scripts/PauseMenu.js
@@ -0,0 +1,44 @@
+﻿#pragma strict
+
+var isPause = false;
+var PauseMenu : Rect = Rect(10,10,200, 200);
+
+function Start () {
+
+}
+
+function Update () {
+	if(Input.GetKeyDown(KeyCode.Escape)){
+		isPause = !isPause;
+		if(isPause){
+			Time.timeScale = 0;
+		}
+		else{
+			Time.timeScale = 1;
+		}
+		
+	}
+}
+
+function OnGUI(){
+	if(isPause){
+		GUI.Window(0, PauseMenu, ThePauseMenu, "Pause Menu");
+	}
+}
+
+function ThePauseMenu(){
+	if(GUILayout.Button("Main Menu")){
+		Application.LoadLevel("MainMenu");
+	}
+	if(GUILayout.Button("Restart")){
+		Application.LoadLevel("MiniGame");
+	}
+	if(GUILayout.Button("Quit")){
+		Application.Quit();
+	}
+	if(GUILayout.Button("Cancel")){
+		//when pressing the escape button again it
+		// closes the windows, but it should work with a button too.
+	}
+	
+}
