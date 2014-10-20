8de5706a5adddb47eab6be1b1e3ae50aed8829f5
amarag <username@users.noreply.github.com>
2014-10-20 22:10:16 +0200

    fixed weird cancel bug

--- Roll-a-ball-Gijs/Assets/Scripts/PauseMenu.js
+++ Roll-a-ball-Gijs/Assets/Scripts/PauseMenu.js
@@ -4,41 +4,19 @@ var isPause = false;
 var PauseMenu : Rect = Rect(10,10,200, 200);
 var oldTimeScale = 5;
 var hide = true;
-//var oldHide = false;
+
+function Start(){
+	oldTimeScale = 5;
+}
 
 function Update () {
 	Debug.Log(hide + ", " + isPause + ", " + oldTimeScale);
-	
 	if(Input.GetKeyDown(KeyCode.Escape) && oldTimeScale == 5){
 		isPause = true;
 		hide = false;
 		oldTimeScale = Time.timeScale;
 		Time.timeScale = 0;
 	}
-	
-	
-	
-	
-	/*
-	if(Input.GetKeyDown(KeyCode.Escape)){
-		isPause = !isPause;
-		if(isPause && hide == false){
-			oldTimeScale = Time.timeScale;
-			Time.timeScale = 0;
-		}
-		else{
-			Time.timeScale = oldTimeScale;
-		}
-	}
-	if(!isPause){
-		oldHide = hide;
-		hide = false;
-	}
-	if(oldHide == false && hide == true){
-		Time.timeScale = 5;
-	}
-	*/
-	
 }
 
 function OnGUI(){
@@ -58,7 +36,6 @@ function ThePauseMenu(){
 		Application.Quit();
 	}
 	if(GUILayout.Button("Cancel")){
-		//oldHide = hide;
 		hide = true;
 		isPause = false;
 		Time.timeScale = oldTimeScale;
